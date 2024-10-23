9129d9675de424c42e49c3146e1d84aed9e21ea9
amit-singh-mobcoder <amit.s@mobcoder.com>
2024-10-23 16:27:20 +0530

    improve overall code and added connection close method

--- src/config/db-connection.ts
+++ src/config/db-connection.ts
@@ -1,11 +1,14 @@
 import mongoose from 'mongoose'
-import { Constants } from '../constants';
+import logger from '../utils/logger';
 
-class DatabaseConnection {
+export class DatabaseConnection {
     private DB_URI:string;
     private DB_NAME: string;
 
     constructor(dbUri: string, dbName: string){
+        if (!dbUri || !dbName) {
+            throw new Error('DB_URI and DB_NAME must be provided.');
+        }
         this.DB_URI = dbUri;
         this.DB_NAME = dbName;
     }
@@ -13,14 +16,24 @@ class DatabaseConnection {
     async connect(){
         try {
             const connectionInstance = await mongoose.connect(`${this.DB_URI}/${this.DB_NAME}`);
-            console.log(`Db connected successfully, DB_HOST: ${connectionInstance.connection.host}, DB_NAME: ${connectionInstance.connection.name}`)
+            logger.info('Db connected successfully')
+            logger.info(`DB-HOST : ${connectionInstance.connection.host}`)
+            logger.info(`DB-NAME : ${connectionInstance.connection.name}`)
         } catch (error) {
-            console.error('Database connection failed', error);
+            logger.error('Database connection failed', error);
             process.exit(1)
+        }
+    }
+
+    async disconnect(){
+        try {
+            await mongoose.connection.close()
+            logger.info('Db connection closed')
+        } catch (error) {
+            logger.error('Error while closing db connection',error)
         }
     }
 }
 
-export default new DatabaseConnection(Constants.DB_URI!, Constants.DB_NAME)
 
 
